d8da65fc8027b6e1103a5e4e812b175060b9d9f1
Eggbertx <eggbertx@users.noreply.github.com>
2023-11-20 10:22:16 -0800

    Add logger test (still incomplete)

--- test/test_console.gd
+++ test/test_console.gd
@@ -4,7 +4,7 @@ extends GdUnitTestSuite
 @warning_ignore('return_value_discarded')
 
 const _console_scene = "res://addons/panku_console/console.tscn"
-const _shell_signal = "toggle_console_action_just_pressed"
+const _shell_signal = "toggle_console_action_just_presseds"
 const _shell_module_name = "interactive_shell"
 
 var console:PankuConsole
@@ -14,23 +14,21 @@ var _console_runner: GdUnitSceneRunner
 func before():
 	console = get_node(PankuConsole.SingletonPath)
 	_console_runner = scene_runner(_console_scene)
-	auto_free(console)
 	auto_free(_console_runner)
 
 
 func test_shell_toggle() -> void:
 	var shell: PankuModuleInteractiveShell = console.module_manager.get_module(_shell_module_name)
-	auto_free(shell)
 	assert_object(shell).is_not_null()
 	
 	# simulate ~ and check that the signal was fired and the
 	# shell is open
-	_console_runner.simulate_key_press(KEY_QUOTELEFT)
-	_console_runner.await_signal_on(console, _shell_signal)
+	_console_runner.simulate_key_pressed(KEY_QUOTELEFT)
+	_console_runner.await_signal(_shell_signal)
 	assert_bool(shell._is_gui_open).is_true()
 	
 	# simulate ~ then check that the signal was fired and the
 	# shell is now closed
-	_console_runner.simulate_key_press(KEY_QUOTELEFT)
-	_console_runner.await_signal_on(console, _shell_signal)
+	_console_runner.simulate_key_pressed(KEY_QUOTELEFT)
+	_console_runner.await_signal(_shell_signal)
 	assert_bool(shell._is_gui_open).is_false()
---
+++ test/test_logger.gd
@@ -0,0 +1,48 @@
+class_name PankuNativeLoggerTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+const _console_scene = "res://addons/panku_console/console.tscn"
+const _shell_module_name = "interactive_shell"
+const _logger_module_name = "native_logger"
+
+var console:PankuConsole
+var _console_runner: GdUnitSceneRunner
+var _console_ui_runner: GdUnitSceneRunner
+var shell: PankuModuleInteractiveShell
+var logger: PankuModuleNativeLogger
+
+func before():
+	console = get_node(PankuConsole.SingletonPath)
+	shell = console.module_manager.get_module(_shell_module_name)
+	logger = console.module_manager.get_module(_logger_module_name)
+	_console_runner = scene_runner(_console_scene)
+	_console_ui_runner = scene_runner("res://addons/panku_console/modules/interactive_shell/console_ui/panku_console_ui.tscn")
+	auto_free(_console_runner)
+	auto_free(_console_ui_runner)
+
+# func after():
+# 	if shell._is_gui_open:
+# 		_console_runner.simulate_key_pressed(KEY_QUOTELEFT)
+
+func test_native_logger():
+	assert_object(console).is_not_null()
+	assert_object(shell).is_not_null()
+	assert_object(logger).is_not_null()
+	assert_object(logger.native_logs_monitor).is_not_null()
+	_console_runner.simulate_key_press(KEY_QUOTELEFT)
+	assert_bool(shell._is_gui_open).is_true()
+
+	_console_ui_runner.simulate_key_pressed(KEY_C)
+	await await_idle_frame()
+	_console_ui_runner.simulate_key_pressed(KEY_C)
+	await await_idle_frame()
+	# var ba = "general_settings.open()".to_ascii_buffer()
+	# for b in ba:
+	# 	_console_ui_runner.simulate_key_pressed(KEY_C)
+	# 	await await_idle_frame()
+
+	# print("Hello, logger!")
+	# _console_runner.await_signal_on(logger.native_logs_monitor, "info_msg_received", ["Hello, loggers!"])
+
